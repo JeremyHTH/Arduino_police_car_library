7cdce56437763d723a2f99e8124e5dedcab6b5ba
Tsz Him Hung <th2hung@uwaterloo.ca>
2026-02-10 21:26:47 -0500

    update

--- library/src/police_car_library.h
+++ library/src/police_car_library.h
@@ -3,4 +3,15 @@
 
 #include <Arduino.h>
 
+class PoliceCar 
+{
+    
+private:
+    // Timer1 (tone)
+    static void _setupTimer(int TimerIndex, float period_s);
+    static void _disableTimer(int TimerIndex);
+
+
+}
+
 #endif // POLICE_CAR_LIBRARY_H
